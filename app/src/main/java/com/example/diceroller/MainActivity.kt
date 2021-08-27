aa9aa56cfab02dafe11388ee0e7777eee3413979
Gledson Luan <gledsonlscruz@gmail.com>
2021-08-27 15:12:17 -0300

    27/08 att

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,59 +1,56 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
+
         //Declare animation and views
         val ttb = AnimationUtils.loadAnimation(this,R.anim.ttb)
         val stb = AnimationUtils.loadAnimation(this,R.anim.stb)
+        val rollButton : Button = findViewById(R.id.rollbutton)
+        val diceImage: ImageView = findViewById(R.id.diceImage)
 
-        val logo = findViewById(R.id.logo) as ImageView
-        val rollButton: Button = findViewById(R.id.button)
-        val diceImage: ImageView = findViewById(R.id.imageView)
-
-
-        //Set The Animation and click button
-        logo.startAnimation(ttb)
-        rollButton.startAnimation(stb)
+        //set Animation and functions
         diceImage.startAnimation(stb)
-
-        rollButton.setOnClickListener {rollDice()}
+        rollButton.startAnimation(ttb)
+        rollButton.setOnClickListener{
+            rollDice()
+            val shake = AnimationUtils.loadAnimation(this,R.anim.shake)
+            diceImage.startAnimation(shake)}
 
         rollDice()
+
     }
+    //fun of the dice
+    private fun rollDice(){
 
-
-    //Object The Dice
-    private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        val diceImage: ImageView = findViewById(R.id.imageView)
-
-        when (diceRoll) {
+        val roll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+        when(roll) {
             1 -> diceImage.setImageResource(R.drawable.dice_1)
             2 -> diceImage.setImageResource(R.drawable.dice_2)
             3 -> diceImage.setImageResource(R.drawable.dice_3)
             4 -> diceImage.setImageResource(R.drawable.dice_4)
             5 -> diceImage.setImageResource(R.drawable.dice_5)
             6 -> diceImage.setImageResource(R.drawable.dice_6)
+            }
         }
-
-    }
-
-    //Plant The Dice
-    class Dice(private val numSides: Int) {
-
-        fun roll(): Int {
+    //instructions to make the dice
+    class Dice(private val numSides: Int){
+        fun roll(): Int{
             return (1..numSides).random()
         }
     }
---
+++ app/src/main/java/com/example/diceroller/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diceroller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            // Delay and Start Activity
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.slide_out_right)
+            finish()
+
+        }, 2000)
+    }
+}
